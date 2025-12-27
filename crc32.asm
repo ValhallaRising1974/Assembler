687238509d53c27cc4df5970abe936faa2711be1
Marcelo dos Santos Prado <msp.berzerker@gmail.com>
2025-12-27 18:35:26 -0300

    Add initial directory structure for lyra-integrity-kit

---
+++ crc32.asm
@@ -0,0 +1,12 @@
+lyra-integrity-kit/
+├─ include/
+│  └─ lyra_integrity.h
+├─ src/
+│  ├─ lyra_crc32c.c
+│  ├─ x86_64/
+│  │  └─ crc32c_hw.asm
+│  └─ arm64/
+│     └─ crc32c_hw.S
+├─ tests/
+│  └─ test_crc32c.c
+└─ Makefile
